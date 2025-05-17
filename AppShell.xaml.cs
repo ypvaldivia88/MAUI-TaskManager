d6a5340fd37e9b7aca80d2848da3317ac57f63ff
Yasmani Palmero Valdivia <yasmani.palmero@ntsprint.com>
2025-05-17 10:37:14 -0400

    Add project files.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskManager
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using TaskManager.ViewModel;
+
+namespace TaskManager
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage(MainViewModel vm)
+        {
+            InitializeComponent();
+            BindingContext = vm;
+        }
+    }
+
+}
